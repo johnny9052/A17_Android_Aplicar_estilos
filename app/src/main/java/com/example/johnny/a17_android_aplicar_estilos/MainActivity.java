ee7a058f9266f3c3dfced4c6753ba3d82dfd8c2b
johnny9052 <alexander9052@gmail.com>
2017-01-10 18:58:15 -0500

    Initial commit

---
+++ app/src/main/java/com/example/johnny/a17_android_aplicar_estilos/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.johnny.a17_android_aplicar_estilos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // En la carpeta values se define un xml style
+
+        // Para aplicar un tema a toda una aplicación edita el archivo
+        // AndroidManifest.xml y agrega el parámetro android:theme en la
+        // etiqueta application:
+
+    }
+}
